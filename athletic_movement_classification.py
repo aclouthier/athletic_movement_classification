f245685cf92bfd4abc31464e220e2eb251cae5d9
Allison Clouthier <56362522+aclouthier@users.noreply.github.com>
2019-11-18 11:14:16 -0500

    Update athletic_movement_classification.py

--- athletic_movement_classification.py
+++ athletic_movement_classification.py
@@ -8,6 +8,9 @@ for three athletes performing 13 athletic tasks.
 This code automatically classifies the movement being performed in each frame 
 of data using DNNs previously trained on subsets of the sIMU data.
 
+DNN is based on: 
+	Ordóñez, F. J., and Roggen, D. (2016). Deep convolutional and LSTM recurrent neural networks 
+	for multimodal wearable activity recognition. Sensors (Switzerland) 16, 115. doi:10.3390/s16010115.
 
 Tested using:
     Python 3.7.3
